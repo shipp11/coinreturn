9452971e347b53b4d7f1f7e71d88fbd8e9d0ecd6
shipp11 <shippdaddy1123@gmail.com>
2017-12-02 15:05:11 -0500

    coinreturn1

--- coinchanger_test.rb
+++ coinchanger_test.rb
@@ -5,4 +5,11 @@ class TestCoinChanger < Minitest::Test
     def test_assert_that_1_equals_1
 		assert_equal(1, 1)
 	end
+
+	def test_assert_that_0_returns_an_empty_hash
+		change = 0
+		assert_equal({}, change_maker_3000(change))
+	end
+
+
 end
